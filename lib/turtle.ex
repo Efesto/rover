dddad18379d1fddd2cf75e66f4f9f8925c04c620
Marco Polita <marco.polita@solarisbank.de>
2020-12-08 18:56:41 +0100

    full implementation

--- lib/turtle.ex
+++ lib/turtle.ex
@@ -4,43 +4,58 @@ defmodule Turtle do
     |> execute(input)
   end
 
-  defp execute(%{direction: "North", position: %{x: x, y: y}}, "M") do
+  defp execute(turtle, "M") do
+    turtle
+    |> move()
+  end
+
+  defp execute(turtle, command) when command in ["L", "R"] do
+    turtle
+    |> rotate(command)
+  end
+
+  defp rotate(%{direction: direction, position: position}, "L") do
+    %{direction: left(direction), position: position}
+  end
+
+  defp rotate(%{direction: direction, position: position}, "R") do
+    %{direction: right(direction), position: position}
+  end
+
+  defp move(%{direction: "South", position: %{x: x, y: y}}) do
+    %{direction: "South", position: %{x: x, y: y - 1}}
+  end
+
+  defp move(%{direction: "West", position: %{x: x, y: y}}) do
+    %{direction: "West", position: %{x: x - 1, y: y}}
+  end
+
+  defp move(%{direction: "East", position: %{x: x, y: y}}) do
+    %{direction: "East", position: %{x: x + 1, y: y}}
+  end
+
+  defp move(%{direction: "North", position: %{x: x, y: y}}) do
     %{direction: "North", position: %{x: x, y: y + 1}}
   end
 
-  defp execute(turtle, "M") do
-   turtle
-   |> move()
+  @directions ["North", "East", "South", "West"]
+
+  defp direction_index(direction) do
+    {_, dir_index} =
+      @directions
+      |> Enum.with_index()
+      |> Enum.find(fn {dir, _} -> dir == direction end)
+
+    dir_index
   end
 
-  defp move(%{direction: direction, position: %{x: x, y: y}}) when direction in ["East", "West"] do
-    if direction == "West" do
-      %{direction: direction, position: %{x: x - 1, y: y}}
-    else
-      %{direction: direction, position: %{x: x + 1, y: y}}
-    end
+  defp left(direction) do
+    @directions
+    |> Enum.at(rem(direction_index(direction) - 1, Enum.count(@directions)))
   end
 
-  defp execute(%{direction: "North", position: %{x: x, y: y}}, "L") do
-    %{direction: "West", position: %{x: x, y: y}}
-  end
-
-  defp execute(%{direction: "West", position: %{x: x, y: y}}, "R") do
-    %{direction: "North", position: %{x: x, y: y}}
-  end
-
-  defp execute(%{direction: "North", position: %{x: x, y: y}}, "R") do
-    %{direction: "East", position: %{x: x, y: y}}
-  end
-end
-
-defmodule TurtleBatcher do
-  def loop(turtle, ["Q"|_]) do
-    # I'm done
-  end
-
-  def loop(turtle, [last|others]) do
-    Turtle.command(turtle, last)
-    |> loop(others)
+  defp right(direction) do
+    @directions
+    |> Enum.at(rem(direction_index(direction) + 1, Enum.count(@directions)))
   end
 end
--- test/turtle_test.exs
+++ test/turtle_test.exs
@@ -1,27 +1,48 @@
 defmodule TurtleTest do
   use ExUnit.Case
-  @initial_turtle %{
-    direction: "North",
-    position: %{x: 0, y: 0}
-  }
 
-  test "M command" do
-    @initial_turtle
-    |> Turtle.command("M")
-    |> assert_turtle_is(%{position: %{x: 0, y: 1}, direction: "North"})
-  end
+  [
+    {"North", %{x: 0, y: 1}},
+    {"West", %{x: -1, y: 0}},
+    {"East", %{x: 1, y: 0}},
+    {"South", %{x: 0, y: -1}}
+  ]
+  |> Enum.each(fn {direction, position} ->
+    @expected_position position
+    @expected_direction direction
+    @turtle %{direction: direction, position: %{x: 0, y: 0}}
 
-  test "M command - West" do
-    %{
-      direction: "West",
-      position: %{x: 0, y: 0}
-    }
-    |> Turtle.command("M")
-    |> assert_turtle_is(%{position: %{x: -1, y: 0}, direction: "West"})
-  end
+    test "when facing #{@expected_direction}, move to expected position" do
+      @turtle
+      |> Turtle.command("M")
+      |> assert_turtle_is(%{position: @expected_position, direction: @expected_direction})
+    end
+  end)
+
+  @directions ["North", "West", "South", "East"]
+
+  @directions
+  |> Enum.with_index()
+  |> Enum.each(fn {direction, i} ->
+    @direction direction
+    @expected_left Enum.at(@directions, rem(i + 1, Enum.count(@directions)))
+    @expected_right Enum.at(@directions, rem(i - 1, Enum.count(@directions)))
+
+    test "when facing #{@direction} rotate left to #{@expected_left}" do
+      %{direction: @direction, position: %{x: 0, y: 0}}
+      |> command("L")
+      |> assert_turtle_is(%{position: %{x: 0, y: 0}, direction: @expected_left})
+    end
+
+    test "when facing #{@direction} rotate right to #{@expected_right}" do
+      %{direction: @direction, position: %{x: 0, y: 0}}
+      |> command("R")
+      |> assert_turtle_is(%{position: %{x: 0, y: 0}, direction: @expected_right})
+    end
+  end)
 
   test "fixture" do
-    @initial_turtle
+    %{direction: "North", position: %{x: 0, y: 0}}
     |> command("M")
     |> assert_turtle_is(%{position: %{x: 0, y: 1}, direction: "North"})
     |> command("L")
@@ -36,11 +57,10 @@ defmodule TurtleTest do
     |> assert_turtle_is(%{position: %{x: -2, y: 1}, direction: "East"})
     |> command("M")
     |> assert_turtle_is(%{position: %{x: -1, y: 1}, direction: "East"})
-#    |> command("?")
-#    |> assert_turtle_is(%{position: %{x: -1, y: 1}, direction: "East"})
+
+    #    |> command("?")
+    #    |> assert_turtle_is(%{position: %{x: -1, y: 1}, direction: "East"})
   end
-
-
 
   def assert_turtle_is(turtle, expectation) do
     assert expectation == turtle
